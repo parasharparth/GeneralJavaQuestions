cafa2371e81ebdd5749d5417b3f01a4114065a5f
parasharparth <parthparashar6@gmail.com>
2023-01-27 23:55:29 -0800

    Jan-27-Reversing a - list

--- src/Lists/jan27/ReverseAList.java
+++ src/Lists/jan27/ReverseAList.java
@@ -1,9 +1,10 @@
 package Lists.jan27;
 
+import java.util.ArrayList;
 public class ReverseAList {
 
     public static void main(String[] args)
     {
-
+        ArrayList<Integer> list = new ArrayList<>(10);
     }
 }
