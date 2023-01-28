86b5f472c6d60534e302fff5f6b15bd506ef5b52
parasharparth <parthparashar6@gmail.com>
2023-01-28 04:19:13 -0800

    Jan-28-List-Basics

---
+++ src/Lists/jan28/ListBasics.java
@@ -0,0 +1,23 @@
+package Lists.jan28;
+import java.util.ArrayList;
+
+public class ListBasics {
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<>(10);
+        for(int i=0; i<10; i++)
+        {
+            list.add(i);
+        }
+        ArrayList<Integer> list1 = new ArrayList<>(list.size());
+        for(int i:list)
+        {
+            list1.add(list.get(i));
+        }
+        for(int i:list1)
+        {
+            System.out.print(list1.get(i)+" ");
+        }
+    }
+}
