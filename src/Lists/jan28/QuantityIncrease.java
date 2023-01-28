a20860ee82fc9a44163a53f0e0ad1fb9ce8eeda8
parasharparth <parthparashar6@gmail.com>
2023-01-28 04:19:31 -0800

    Jan-28-Quantity-Increase

---
+++ src/Lists/jan28/QuantityIncrease.java
@@ -0,0 +1,18 @@
+package Lists.jan28;
+import java.util.ArrayList;
+
+/************************************************************************************************************************
+ * Default Quantity of ArrayList is 10.
+ * ensureCapacity for increasing capacity to ensure a certain number of elements can be held there.
+ * This is true for streaming environments where there are a lot of elements present.
+ * list.size() gives the CURRENT SIZE and not the default allocated size for the arraylist or for that matter any list
+ ************************************************************************************************************************/
+public class QuantityIncrease {
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.ensureCapacity(20);
+        System.out.println(list.size());
+    }
+}
