fa452031a4c38fd43f4d67e2fe009df24246ff40
parasharparth <parthparashar6@gmail.com>
2023-01-29 22:28:48 -0800

    ReverseAList

---
+++ src/Lists/jan29/ReverseAList.java
@@ -0,0 +1,24 @@
+package Lists.jan29;
+
+import java.util.ArrayList;
+
+public class ReverseAList {
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<10; i++)
+        {
+            list.add(i);
+        }
+        ArrayList<Integer> relist = new ArrayList<>();
+        for(int i=list.size()-1; i>=0; i--)
+        {
+            relist.add(list.get(i));
+        }
+        for(int i=0; i<relist.size(); i++)
+        {
+            System.out.println(relist.get(i));
+        }
+    }
+}
