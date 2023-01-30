e1a67e213b4a026c0e8657e3200d58ff9c2b64b0
parasharparth <parthparashar6@gmail.com>
2023-01-29 22:28:22 -0800

    List Empty or not

---
+++ src/Lists/jan29/ListEmptyOrNot.java
@@ -0,0 +1,12 @@
+package Lists.jan29;
+
+import java.util.ArrayList;
+
+public class ListEmptyOrNot {
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(list.isEmpty());
+    }
+}
